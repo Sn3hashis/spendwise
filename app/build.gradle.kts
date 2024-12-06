e0f95c459767af8f8f0b4f27efefa49d23e018cd
snehashis <sn3hashis@gmail.com>
2024-12-06 09:59:11 +0530

    added google service.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
